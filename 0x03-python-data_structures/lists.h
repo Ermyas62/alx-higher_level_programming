f520be0d9bee8bd6c3199289744b57fa793268b1
Ermyas62 <ermyas62@gmail.com>
2023-12-05 10:18:04 -0800

    Delete at

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -26,6 +26,7 @@ add_tuple(tuple_a=(), tuple_b=());
 multiple_returns(sentence);
 max_integer(my_list=[]);
 divisible_by_2(my_list=[]);
+delete_at(my_list=[], idx=0);
 
 int is_palindrome(listint_t **head);
 
