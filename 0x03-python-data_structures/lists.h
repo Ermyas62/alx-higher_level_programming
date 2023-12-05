185ae45c16fb387d14c2524db9bf0e59aa6726e4
Ermyas62 <ermyas62@gmail.com>
2023-12-05 05:34:18 -0800

    Print a list of integers... in reverse!

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,6 +18,8 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+replace_in_list(my_list, idx, element);
+print_reversed_list_integer(my_list=[]);
 
 int is_palindrome(listint_t **head);
 
