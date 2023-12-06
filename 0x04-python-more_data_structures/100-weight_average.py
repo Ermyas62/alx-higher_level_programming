7125f7b74b0879f31db3cd5ea7ba2904eae15e2c
Ermyas62 <ermyas62@gmail.com>
2023-12-06 04:57:43 -0800

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list:
-        weight = 0
-        denom = 0
-        avg = 0
-
-        for i in range(len(my_list)):
-            for j in range(len(my_list[i])):
-                weight += my_list[i][0] * my_list[i][1]
-                denom += my_list[i][1]
-            avg = weight / denom
-            return avg
+    if len(my_list) == 0:
         return 0
+    return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
+def mul(x, y):
+    return x * y
