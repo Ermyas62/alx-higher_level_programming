e1c16cdf7557181e90256255026e57804e0cb864
Ermyas62 <ermyas62@gmail.com>
2023-12-06 03:18:12 -0800

    Delete by value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,14 @@
+#usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keylist = []
+    if value in a_dictionary.values():
+        for key, val in a_dictionary.items():
+            if value == val:
+                keylist.append(key)
+
+        for k1 in keylist:
+            if k1 in a_dictionary:
+                del a_dictionary[k1]
+    return a_dictionary
+
+
