ab9b1e8732a3e629f959e7dfb7817f70809c5d90
Ermyas62 <ermyas62@gmail.com>
2024-01-07 10:25:35 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
         decrements with every deletion
     """
 
-    number_of_instances = 1
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """ Intialize a rectangle instance.
@@ -23,6 +23,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         """ returns an informal and nicely printable string representation
