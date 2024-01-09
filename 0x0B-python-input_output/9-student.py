f3f0600dfa8f365a3b40499526f6433c7ea48245
Ermyas62 <ermyas62@gmail.com>
2024-01-09 09:55:18 -0800

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The class "student" container
+"""
+
+
+class Student:
+    """representation of a stuedent"""
+    def __int__(self, first_name, last_name, age):
+        """intializes the students"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of a student instance"""
+        return self.__dict__
