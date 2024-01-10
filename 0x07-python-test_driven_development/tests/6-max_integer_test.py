e4a380c68efd4e5ef9db8b03edaa7a87323bb096
Ermyas62 <ermyas62@gmail.com>
2024-01-10 12:04:30 -0800

    Max integer - Unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
+""" Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-""" module to find the max integer in a list """
 
+class TestMaxInteger(unittest, TestCase):
+    """Unit test for max_integer """
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-    if the list is empty, the function returns None
-    """
+    def test_positive(self):
+        """test positive values """
+        self.assertEqual(max_integer([50, 100, 25]), 100)
 
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    def test_negative(self):
+        """test negative values """
+        self.assertEqual(max_integer([-50, -100, -25]), -25)
+
+    def test_mix(self):
+        """test mix values """
+        self.assertEqual(max_integer([50, -100, 25]), 50)
+
+    def test_empty_list(self):
+        """test empty list """
+        self.assertEqual(max_integer([]), None)
+
+    def test_dictionary(self):
+        """ test dictionary """
+        self.assertRaises(KeyError, max_integer, {5: 'a', 4: 'b', 3: 'c'})
+
+    def test_one(self):
+        """ test one element in list """
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_string(self):
+        """ test string """
+        self.assertEqual(max_integer('hello'), 'o')
