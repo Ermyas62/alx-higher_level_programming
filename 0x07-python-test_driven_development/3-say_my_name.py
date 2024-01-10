a9b8158b2d6875ff4eec5f290a906ddaa47dd8bc
Ermyas62 <ermyas62@gmail.com>
2024-01-10 12:07:14 -0800

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,7 @@ this is the "3-say_my_name" module.
 the 3-say_my_name module supplies on efunction, say_my_name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     prints "my name is" followed by the first name and last name"""
