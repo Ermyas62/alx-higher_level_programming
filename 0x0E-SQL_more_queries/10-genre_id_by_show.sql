7f2624e58700d58864bda5f9753af202fe396229
Ermyas62 <ermyas62@gmail.com>
2024-02-14 05:22:02 -0800

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
 -- Lists all shows that have at least one gener
 -- Records are sorted by ascending tv_shows.title
-SELECT s.`title`, g.`genre_id`
-FROM `tv.shows` AS s
-	INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
-ORDER BY s.`title`, g.`genre_id`;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv.shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
