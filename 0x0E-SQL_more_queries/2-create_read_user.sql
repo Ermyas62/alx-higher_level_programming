626933ac427a2b4164232fb79796dce31cf28b0e
Ermyas62 <ermyas62@gmail.com>
2024-02-14 05:38:30 -0800

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
--- Create database
--- Create user
+-- Create database and the user
+-- If the user exist, script fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `htbn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES
