b90036aabfa2c270f1e8f5564812100d27a4161c
Ermyas62 <ermyas62@gmail.com>
2024-02-14 04:47:10 -0800

    By genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,5 +7,5 @@ COUNT(*) AS `number_of_shows`
 FROM `tv_genres` AS g
 	INNER JOIN `tv_show_genres` AS t
 	ON g.`id` = t.`genre_id`
-group by g.`name`
-ORDER BY `number_of_shows` DESC;	
+GROUP BY g.`name`
+ORDER BY `number_of_shows` DESC;
