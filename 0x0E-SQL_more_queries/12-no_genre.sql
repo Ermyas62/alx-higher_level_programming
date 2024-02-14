40022a2db511a062c8f2081e5d18f2c706897872
Ermyas62 <ermyas62@gmail.com>
2024-02-13 23:13:05 -0800

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- List all shows in the database without a genre linked
+-- Records are ordered by ascending tv_shows.title
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+	LEFT JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+	WHERE g.`genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
