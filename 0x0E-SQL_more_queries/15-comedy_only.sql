51b823e6571fde2c4392904c42bc4d79dcf977ab
Ermyas62 <ermyas62@gmail.com>
2024-02-14 04:42:26 -0800

    comedy only

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,7 @@
 -- Lists all comedy shows
 -- Records are ordered by descending show title
-SELECT t.`title`
-FROM `tv_shows` AS t
-	INNER JOIN `tv_show_genres` AS s
-	ON t.`id` = s.`show_id`
-
-	INNER JOIN `tv-genres` AS g
-	ON g.`id` = s.`genre_id`
-	WHERE g.`name` = "Comedy"
-ORDER BY t.`title`;
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv-genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY title;
