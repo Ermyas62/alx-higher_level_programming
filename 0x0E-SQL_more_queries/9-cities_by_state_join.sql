6c5efe50a116ac7b3f3200dfd6378afc6551f24f
Ermyas62 <ermyas62@gmail.com>
2024-02-14 05:15:01 -0800

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 -- Lists all cities in the database
 -- Records are sorted in order of ascending cities.id
-SELECT c.`id`, c.`name`, s.`name`
-FROM `cities` as c
-	INNER JOIN `states` AS S
-	ON c.`state_id` = s.`id`
-ORDER BY c.`id`;
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
