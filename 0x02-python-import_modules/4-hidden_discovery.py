4e76526dc01431e992afc2cd75d7496748f15c90
Ermyas62 <ermyas62@gmail.com>
2024-02-19 23:08:28 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
+    import sys
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "_":
-            print(name)
+    for n in dir(hidden_4):
+        if n[:2] != "_":
+            print(n)
