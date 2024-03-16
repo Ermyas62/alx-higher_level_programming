21a4501225baa782a9a13b1c6e4adc117d622e69
Ermyas62 <ermyas62@gmail.com>
2024-03-16 02:13:01 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,12 +4,11 @@ const supSquare = require('./5-square');
 
 class Square extends supSquare {
 	charPrint (c) {
-		if (c = null) {
-			c= 'X';
-		}
-		for (let i = 0; i < this.width; i++) {
-			console.log(c.repeat(this.width));
-		}
+	  if (c === undefined) {
+	    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
+	  } else {
+	    console.log((c.repeat(this.width) + '\n').repeat(this.height - 1) + c.repeat(this.width));
+	  }
 	}
 }
 
