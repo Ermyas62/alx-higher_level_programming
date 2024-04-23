31c773bf3d977a1d9cc5a0d3f1709a4c37c73faf
Ermyas62 <ermyas62@gmail.com>
2024-04-23 02:19:36 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // A script that prints the title of a Star Wars movie where the 
 // episode number matches a given integer.
+
 const request = require('request');
 
 const episodeId = process.argv[2];
